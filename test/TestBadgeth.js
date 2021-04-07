7de1026b948a9ced6cd910ac4c89bac0ba258b58
Connor Dunham <cjdunham2112@gmail.com>
2021-04-07 10:50:38 -0700

    Changes import from var to const

--- test/TestBadgeth.js
+++ test/TestBadgeth.js
@@ -1,6 +1,8 @@
-var assert = require('assert');
+const assert = require('assert');
 
 const BadgeFactory = artifacts.require("BadgeFactory");
+
+const GQL_QUERY_ITS_ONLY_WAFER_THIN = "{itsOnlyWaferThinBadges(first: 200){\nid\nawardedAtBlock\nawarded"
 
 contract("BadgeFactory", async accounts => {
     it("should assign governance to 1st ethereum account", async () => {
@@ -8,4 +10,15 @@ contract("BadgeFactory", async accounts => {
         const factoryGov = await factory.governance.call();
         assert.strictEqual(accounts[0], factoryGov, "gov not set");
     });
+
+    it("should allow governance to set which subgraph gets queried", async () => {
+        const factory = await BadgeFactory.deployed();
+        factory.setQueryURL("testing").call();
+        assert.strictEqual(factory.queryURL.call(), "testing", "Failed to set queryURL");
+    })
+
+    // it("should emit a {BadgeDefinitionCreated} event when createBadgeDefinitionIsCalled", async () => {
+    //     const factory = await BadgeFactory.deployed();
+    //     factory.createBadgeDefinition("indexer.isOverDelegated", false, true);
+    // })
 });
